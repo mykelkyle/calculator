c7867a2950afade0d9959add4fe38dae9f0b3710
Mykel Murphy <murphy@akane.waseda.jp>
2022-12-19 10:38:52 -0500

    add initial wireframe design and js operators

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const remainder = function (a, b) {
+  return a % b;
+};
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  } else if (operator === "%") {
+    return remainder(num1, num2);
+  }
+}
